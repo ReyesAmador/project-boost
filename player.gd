e3aa7aa165519f2f7da17c34fcc3fd5ad719a7a8
45986076 <reyes.amador@terrassa.cat>
2026-04-01 14:34:36 +0200

    Comit inicial

---
+++ player.gd
@@ -0,0 +1,66 @@
+extends RigidBody3D
+
+## Cuanto sube en vertical aplicando la fuerza
+@export var thrust: float = 1000
+## Cuanta fuerza se aplica para rotar el objeto
+@export var torque_thrust: float = 100
+
+#Se hace rapido arrastrando el nodo, pulsando luego ctr y ponerlo en el script
+@onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
+@onready var success_audio: AudioStreamPlayer = $SuccessAudio
+@onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
+@onready var booster_particles: GPUParticles3D = $BoosterParticles
+@onready var right_booster_particles: GPUParticles3D = $RightBoosterParticles
+@onready var left_booster_particles: GPUParticles3D = $LeftBoosterParticles
+
+var is_transitioning: bool = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("boost"):
+		apply_central_force(basis.y * delta * thrust)
+		booster_particles.emitting = true
+		if !rocket_audio.playing:
+			rocket_audio.play()
+	else:
+		rocket_audio.stop()
+		booster_particles.emitting = false
+	
+	if Input.is_action_pressed("rotate_left"):
+		apply_torque(Vector3(0.0,0.0, torque_thrust * delta))
+		right_booster_particles.emitting = true
+	else:
+		right_booster_particles.emitting = false
+	
+	if Input.is_action_pressed("rotate_right"):
+		apply_torque(Vector3(0.0,0.0, -torque_thrust * delta))
+		left_booster_particles.emitting = true
+	else:
+		left_booster_particles.emitting = false
+
+
+func _on_body_entered(body: Node) -> void:
+	if !is_transitioning:
+		if "Goal" in body.get_groups():
+			complete_level(body.file_path)
+		
+		if "Floor" in body.get_groups():
+			crash_sequence()
+		
+func crash_sequence() -> void:
+	print("BoOoM")
+	explosion_audio.play()
+	set_process(false)
+	is_transitioning = true
+	var tween = create_tween()
+	tween.tween_interval(2.5)
+	tween.tween_callback(get_tree().reload_current_scene)
+	
+func complete_level(next_level_file: String) -> void:
+	success_audio.play()
+	set_process(false)
+	is_transitioning = true
+	var tween = create_tween()
+	tween.tween_interval(1.5)
+	tween.tween_callback(get_tree().change_scene_to_file.bind(next_level_file))
+	
